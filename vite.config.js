c4812c4b2f5c1788de4789b2ec11054452b7188e
Utkarsh Bahuguna <85474312+u7k4rs6@users.noreply.github.com>
2025-04-11 14:45:49 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import App from './App.jsx'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
